f3bddff6f52ccf0d0476ef9a20c83564b9a8d54a
Farshad Falaki <farshad.falaki@gmail.com>
2020-11-02 15:31:41 +0100

    comment sudo part

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ mvn clean package
 
 echo '******Building docker files******'
 echo '**User should have permission to run docker**'
-sudo docker build -t checkout .
+#sudo
+docker build -t checkout .
 
 echo '******Going to run network via docker-compose******'
 echo '**User should have permission to run docker-compose**' 
-sudo docker-compose -f docker-compose.yml up
+#sudo 
+docker-compose -f docker-compose.yml up
 
